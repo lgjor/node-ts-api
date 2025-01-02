977cb7a6be79173afc921a721ca99a4f708ff997
Lucas Godoy <lucasgodoyjor@gmail.com>
2025-01-02 16:02:12 -0300

    Setup Node.js with OvernightJS and express

---
+++ test/globals.d.ts
@@ -0,0 +1,9 @@
+// filepath: /i:/node-ts-api/test/globals.d.ts
+
+// declare var testRequest: import('supertest').SuperTest<import('supertest').Test>;
+
+declare global {
+    var testRequest: import('supertest/lib/agent');
+  }
+  
+  export {};
